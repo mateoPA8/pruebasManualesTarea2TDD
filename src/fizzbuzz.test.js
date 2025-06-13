80883b2c0041f58be491d4805a6941d7cb32aa03
Your Name <mateo.poquechoque@ucb.edu.bo>
2025-06-13 16:12:22 -0400

    devuelve buzz si es 5

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -7,11 +7,17 @@ describe("Fizzbuzz", () => {
   it("deberia devolver fizz si el numero es 3", () => {
     expect(fizzbuzz(3)).toEqual("fizz");
   });
+  it("deberia devolver buzz si el numero es 5", () => {
+    expect(fizzbuzz(5)).toEqual("buzz");
+  });
 });
 
 function fizzbuzz(n){
     if(n==3){
         return "fizz";
+    }
+    else if (n==5){
+        return "buzz";
     }
     return 1;
 }
