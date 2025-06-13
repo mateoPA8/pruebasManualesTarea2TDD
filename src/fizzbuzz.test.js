468d4af1bd0f3dda4e7fee7e83219424538d1875
Your Name <mateo.poquechoque@ucb.edu.bo>
2025-06-13 15:58:08 -0400

    devuelve el numero 1

---
+++ src/fizzbuzz.test.js
@@ -0,0 +1,11 @@
+import sumar from "./sumador.js";
+
+describe("Fizzbuzz", () => {
+  it("deberia devolver el numero 1", () => {
+    expect(fizzbuzz(1)).toEqual(1);
+  });
+});
+
+function fizzbuzz(n){
+    return 1;
+}
