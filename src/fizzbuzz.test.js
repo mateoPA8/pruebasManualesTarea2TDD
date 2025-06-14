22d53d8f3817d029276955064bf372fadf4ddebb
Your Name <mateo.poquechoque@ucb.edu.bo>
2025-06-13 20:11:00 -0400

    refactorizando la funcion

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -15,16 +15,14 @@ describe("Fizzbuzz", () => {
   });
 });
 
-function fizzbuzz(n){
-    if (n%5==0 && n%3==0)
-    {
+function fizzbuzz(n) {
+    if (n % 3 === 0 && n % 5 === 0) {
         return "fizzbuzz";
-    }
-    if(n==3){
+    } else if (n % 3 === 0) {
         return "fizz";
-    }
-    else if (n==5){
+    } else if (n % 5 === 0) {
         return "buzz";
+    } else {
+        return 1;
     }
-    return 1;
 }
